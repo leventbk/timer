b2824858eb0206b78e1958b74548cfcc5ec5f6e6
leventkarakus <leventbkarakus@hotmail.com>
2023-06-12 14:34:16 -0400

    add comment

--- timer1.js
+++ timer1.js
@@ -1,14 +1,17 @@
 
 const parameter = process.argv.slice(2);
-let time = parameter.filter(number => { return number > 0})
+let time = parameter.filter(number => {
+  return number > 0;
+});
 
-const timer = function () {
-	//const argv = process.argv.slice(2)
-	time = time * 1000; 
+const timer = function() {
+  //const argv = process.argv.slice(2)
+  time = time * 1000; //milisecond to second
 	
+  //I've used this code snippet for using system sound for macOS
+  setTimeout(() => {
+    console.log('\n \n !!! Times up !!! \n \n'); process.stdout.write(require("child_process").exec("afplay /System/Library/Sounds/Glass.aiff"));
+  }, time);
 
-	setTimeout(() => { console.log('\n \n !!! Times up !!! \n \n'); process.stdout.write(require("child_process").exec("afplay /System/Library/Sounds/Glass.aiff"))}, time);
-
-
-}
-timer()
+};
+timer();
