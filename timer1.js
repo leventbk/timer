515c227e75648cd0397d61b97e32ae2557a487ba
leventkarakus <leventbkarakus@hotmail.com>
2023-06-14 12:34:24 -0400

    CR > map func. added, process.stdout write removed

--- timer1.js
+++ timer1.js
@@ -1,16 +1,18 @@
-
 const parameter = process.argv.slice(2);
+
 let time = parameter.filter(number => {
   return number > 0;
 });
 
 const timer = function() {
   //const argv = process.argv.slice(2)
-  time = time * 1000; //milisecond to second
-	
+  //time = time * 1000; //milisecond to second
+	time = time.map(number => number * 1000);
+
   //I've used this code snippet for using system sound for macOS
   setTimeout(() => {
-    console.log('\n \n !!! Times up !!! \n \n'); process.stdout.write(require("child_process").exec("afplay /System/Library/Sounds/Glass.aiff"));
+    console.log('\n \n !!! Times up !!! \n \n'); 
+    require("child_process").exec("afplay /System/Library/Sounds/Glass.aiff");
   }, time);
 
 };
